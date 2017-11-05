07925bc7e940e132c1dd35c4ea75e87b5e36f202
Hubert Maćkowiak <mackowiak.h@wp.pl>
2017-11-05 23:02:37 +0100

    scripts js file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames); //Then we have all names in new table
+
+console.log(allNames);
+
+var newName = "Marian";
+
+if (allNames.indexOf(newName) === -1) {
+    var allNames = allNames.push(newName); // At this momen tabel "allNames" has new long equal 9
+    console.log(allNames);    
+}
